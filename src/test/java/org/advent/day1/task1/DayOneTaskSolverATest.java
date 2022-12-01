48597eab6e5518a28e087e77e325505ff1b821b4
Zoltan Lajtavari <zoltan.lajtavari@idealo.de>
2022-12-01 18:08:31 +0100

    improves test by using a wider variety of test data

--- src/test/java/org/advent/day1/task1/DayOneTaskSolverATest.java
+++ src/test/java/org/advent/day1/task1/DayOneTaskSolverATest.java
@@ -1,5 +1,6 @@
 package org.advent.day1.task1;
 
+import org.advent.util.FileLoader;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,12 +10,12 @@ class DayOneTaskSolverATest {
     @Test
     void shouldFindMax() {
         // given
-        final var input = "1\n2\n\n6\n\n5\n6\n7\n";
+        final var input = FileLoader.loadFile("/org/advent/day1/task1/day_1_test_input.txt");
 
         // when
         final var calculatedMax = DayOneTaskSolverA.findMax(input);
 
         // then
-        assertEquals(18, calculatedMax);
+        assertEquals(24000, calculatedMax);
     }
 }
