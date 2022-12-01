dba467ef515449c6598f9924ecf9702c05d94d5b
Zoltan Lajtavari <zoltan.lajtavari@idealo.de>
2022-12-01 18:03:34 +0100

    implementents calculation for day 1 task 2

--- src/main/java/org/advent/Main.java
+++ src/main/java/org/advent/Main.java
@@ -1,11 +1,13 @@
 package org.advent;
 
 import org.advent.day1.task1.DayOneTaskSolverA;
+import org.advent.day1.task1.DayOneTaskSolverB;
 
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.printf("Find the Elf carrying the most Calories. How many total Calories is that Elf carrying? Answer: %s%n", DayOneTaskSolverA.solveTask());
+        System.out.printf("Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total? Answer: %s%n", DayOneTaskSolverB.solveTask());
     }
 }
